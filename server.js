3f8211e10de4f5e098b50dadec632b3e5ed9783d
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-03 17:37:18 +0100

    chore: Update SOAP server implementation for weather service

--- server.js
+++ server.js
@@ -1,31 +1,83 @@
 const soap = require('soap');
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Define the service implementation
-const service = {
-  WeatherService: {
-    WeatherPort: {
-      GetWeather: function (args, callback) {
-        // Perform the logic to get the weather based on the location
-        const location = args.location;
-        const temperature = '25°C';
-        const description = 'Sunny';
-
-        // Return the response
-        const result = {
-          temperature: temperature,
-          description: description,
+var myService = {
+  MyService: {
+    MyPort: {
+      MyFunction: function (args) {
+        return {
+          name: args.name,
         };
-        callback(null, result);
+      },
+
+      // This is how to define an asynchronous function with a callback.
+      MyAsyncFunction: function (args, callback) {
+        // do some work
+        callback({
+          name: args.name,
+        });
+      },
+
+      // This is how to define an asynchronous function with a Promise.
+      MyPromiseFunction: function (args) {
+        return new Promise((resolve) => {
+          // do some work
+          resolve({
+            name: args.name,
+          });
+        });
+      },
+
+      // This is how to receive incoming headers
+      HeadersAwareFunction: function (args, cb, headers) {
+        return {
+          name: headers.Token,
+        };
+      },
+
+      // You can also inspect the original `req`
+      reallyDetailedFunction: function (args, cb, headers, req) {
+        console.log(
+          'SOAP `reallyDetailedFunction` request from ' +
+            req.connection.remoteAddress,
+        );
+        return {
+          name: headers.Token,
+        };
       },
     },
   },
 };
 
-// Create the SOAP server
-const xml = require('fs').readFileSync('weatherService.wsdl', 'utf8');
-const server = soap.listen({ path: '/weather', xml: xml }, function () {
-  console.log('SOAP server running at http://localhost:8000/weather?wsdl');
+var xml = require('fs').readFileSync('wsdl/myservice.wsdl', 'utf8');
+
+//http server example
+var server = http.createServer(function (request, response) {
+  response.end('404: Not Found: ' + request.url);
 });
 
-// Attach the service implementation to the SOAP server
-server.addService(xml, service, { suppressStack: true });
+server.listen(8000);
+soap.listen(server, '/wsdl', myService, xml, function () {
+  console.log('server initialized');
+});
+
+//express server example
+var app = express();
+//body parser middleware are supported (optional)
+app.use(
+  bodyParser.raw({
+    type: function () {
+      return true;
+    },
+    limit: '5mb',
+  }),
+);
+app.listen(8001, function () {
+  //Note: /wsdl route will be handled by soap module
+  //and all other routes & middleware will continue to work
+  soap.listen(app, '/wsdl', myService, xml, function () {
+    console.log('server initialized');
+  });
+});
