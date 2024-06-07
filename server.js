b4b05d0ae1a352c5b371eb8a7ab7924c0fa62bc4
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-07 08:24:24 +0100

    chore: Add weather service to SOAP server
    This commit adds the necessary code changes to include the weather service in the SOAP server implementation. It updates the WSDL definition for the weather service and adds the corresponding endpoint to the server. This change is in line with recent user commits that updated the WSDL definition and SOAP server implementation for the weather service.

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const { greeterService } = require('./services/definition/greetUser');
 
 var xml_myService = require('fs').readFileSync('wsdl/myservice.wsdl', 'utf8');
 var xml_greeterService = require('fs').readFileSync('wsdl/greetUser.wsdl', 'utf8');
+var xml_weather = require('fs').readFileSync('wsdl/weatherService.wsdl', 'utf8');
 
-//express server example
 var app = express();
-//body parser middleware are supported (optional)
 app.use(
   bodyParser.raw({
     type: function () {
@@ -21,10 +20,14 @@ app.use(
 app.listen(8000, function () {
 
   soap.listen(app, '/myservice', myService, xml_myService, function () {
-    console.log('server initialized');
+    console.log('server myservice initialized');
   });
 
   soap.listen(app, '/greeter', greeterService, xml_greeterService, function () {
-    console.log('server initialized');
+    console.log('server greeter initialized');
+  });
+
+  soap.listen(app, '/weather', greeterService, xml_weather, function () {
+    console.log('server weather initialized');
   });
 });
